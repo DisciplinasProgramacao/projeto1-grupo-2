3dca18687272904ed3d6b8233876b4b4459bfbe4
MatheusMOAndrade <andradematheusmo@gmail.com>
2022-09-07 16:48:42 -0300

    update class

--- codigo/Date.java
+++ codigo/Date.java
@@ -1,40 +1,14 @@
 
-/** 
- * MIT License
- *
- * Copyright(c) 2022 João Caram <caram@pucminas.br>
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
-/** Classe data simples para fundamentos de POO */
 public class Date {
-    ;
-
+    
     // #region atributos
     // constante: dias de cada mês
     private static final int[] DIASDOMES = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
     private static final int ANO_ATUAL = 2022;
     private static final String[] DIASDASEMANA = { "sábado", "domingo", "segunda", "terça", "quarta", "quinta",
             "sexta" }; // 2022 começa em um sábado
-
-    // atributos
+    
+            // atributos
     private int dia;
     private int mes;
     private int ano;
@@ -42,65 +16,32 @@ public class Date {
 
     // #region Construtores
 
-    /**
-     * Inicializador privado. É utilizado pelos construtores. Faz a validação e, em
-     * caso de data inválida, retorna 01/01/1900.
-     * 
-     * @param dia Dia para a data
-     * @param mes Mês para a data
-     * @param ano Ano para a data
-     */
+    
     private void init(int dia, int mes, int ano) {
         this.dia = dia;
         this.mes = mes;
         this.ano = ano;
-        if (!this.dataValida()) { // se a data não é válida... (método da própria classe)
+        if (!this.dataValida()) { 
             this.dia = this.mes = 1;
             this.ano = 1900;
         }
 
     }
-
-    /**
-     * Construtor completo: recebe dia, mês e ano e valida a data. Datas inválidas
-     * vão para 01/01/1900
-     * 
-     * @param dia Dia para a data
-     * @param mes Mês para a data
-     * @param ano Ano para a data
-     */
+    
     public Date(int dia, int mes, int ano) {
         this.init(dia, mes, ano);
     }
 
-    /**
-     * Construtor para ano atual: recebe dia, mês e completa com ano atual (2022).
-     * Datas inválidas vão para 01/01/1900
-     * 
-     * @param dia Dia para a data
-     * @param mes Mês para a data
-     * 
-     */
+    
     public Date(int dia, int mes) {
         this.init(dia, mes, ANO_ATUAL);
     }
-
-    /**
-     * Construtor para data padrão: 01/01/1900
-     */
+    
     public Date() {
         this.init(1, 1, 1990);
     }
     // #endregion
-
-    /**
-     * Verifica se o ano da data armazenada é bissexto.
-     * Para regras do ano bissexto:
-     * http://educacao.uol.com.br/disciplinas/matematica/ano-bissexto-eles-se-repetem-a-cada-4-anos.htm
-     * http://www.sogeografia.com.br/Curiosidades/?pg=4
-     * 
-     * @return Se o ano é bissexto (true) ou não (false)
-     */
+    
     public boolean anoBissexto() {
         boolean resposta = false;
         if (this.ano % 400 == 0)
@@ -110,13 +51,7 @@ public class Date {
 
         return resposta;
     }
-
-    /**
-     * Verifica se a data armazenada é válida (método privado). Só valem datas de
-     * 1900 em diante.
-     * 
-     * @return TRUE se é válida ; FALSE se não é válida
-     */
+    
     private Boolean dataValida() {
         Boolean resposta = true; // resposta sobre a validade
         int maximoDeDias = 0;
@@ -136,14 +71,7 @@ public class Date {
         }
         return resposta; // retorna a validação obtida
     }
-
-    /**
-     * Método static: calcula do dia da semana de uma data em 2022 (ignora o ano
-     * caso seja diferente).
-     * Datas inválidas retornam e são calculadas como 01/01/2022
-     * 
-     * @return
-     */
+    
     public static String diaDaSemanaEm2022(String data) {
 
         int deslocamento = 0;
@@ -168,20 +96,17 @@ public class Date {
         return DIASDASEMANA[deslocamento];
     }
 
-    /**
-     * Retorna a data formatada
-     * 
-     * @return String com a data no formato dd/mm/aaaa
-     */
+   
     public String dataFormatada() {
 
         return (String.format("%02d", this.dia) + "/" + String.format("%02d", this.mes) + "/"
                 + String.format("%4d", this.ano));
     }
 
-    public String incrementsDate(String date, int quantity) {
+    public String incrementDate(String date, int quantity) {
         int day, month, year;
         String[] detailDate = date.split("/");
+
         day = Integer.parseInt(detailDate[0]);
         month = Integer.parseInt(detailDate[1]);
         year = Integer.parseInt(detailDate[2]);
@@ -194,13 +119,13 @@ public class Date {
             if (aux.mes == 12) {
                 aux.ano++;
                 aux.mes = 1;
-            } else
+            } else {
                  aux.mes++;
+            }
             aux.dia = aux.dia - DIASDOMES[aux.mes];
         }
 
         return dataFormatada();
-
     }
 
 }
--- codigo/Schedule.java
+++ codigo/Schedule.java
@@ -2,16 +2,13 @@ import java.util.ArrayList;
 
 public class Schedule {
 
-    private ArrayList<Task> tasks;
+    private Task task;
+    private static ArrayList<Task> schedule = new ArrayList<>();
 
-    public Schedule(){
-        tasks = new ArrayList<>();
+    public void addTask(Task task) {
+        
+        schedule.add(task);
     }
-
-   public void createTask(String date, String name, String description, int periodicity, int periodicityLimit) {
-      Task newTask = new Task(date, name, description, periodicity, periodicityLimit);
-      tasks.add(newTask); 
-   }
  
     
 }
