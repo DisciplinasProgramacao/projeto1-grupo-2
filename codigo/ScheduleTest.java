4c789306fa4d533918ec28567f31793e6ba43e95
brunobgf <brunobgf40@gmail.com>
2022-09-07 21:17:35 -0300

    adding first tests

--- codigo/ScheduleTest.java
+++ codigo/ScheduleTest.java
@@ -1,8 +1,28 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class ScheduleTest{
+public class ScheduleTest {
+
+    /*
+     * Plano de Testes - Schedule
+     * Criar um novo compromisso ✔️
+     */
+
+    private static Schedule agenda;
+
+    @BeforeAll
+    public static void init() {
+        agenda = new Schedule();
+    }
+
+    @Test
+    public void registraNovaAgenda() {
+        agenda.createAppointment("07/09/2022",
+         "Reunião cá Senior",
+          "Senior salvando o dia ",
+           0,
+            0
+            );
+    }
 
 }
--- codigo/TaskTest.java
+++ codigo/TaskTest.java
@@ -1,4 +1,8 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    
-    
+
+
+
 }
