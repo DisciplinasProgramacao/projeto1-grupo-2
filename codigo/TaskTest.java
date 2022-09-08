b3315bc3eb07d000edbccafb1adf0234589949b9
Paganini <paganini.xxi@gmail.com>
2022-09-07 22:32:05 -0300

    "Task tests"

--- codigo/TaskTest.java
+++ codigo/TaskTest.java
@@ -1,8 +1,27 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TaskTest {
+    static Task task;
 
+    @BeforeEach
+    public void init(){
+        task = new Task("01/01/2022", "Dog walking", "Walking with my dog", 7);
+    }
+
+    //Testing the getters and setters to see if the instance was created right
+    @Test
+    public void testingReturningTaskName(){
+        assertEquals("Dog walking", task.getName());
+    }
+
+    @Test
+    public void testingReturningTaskDescription(){
+        assertEquals("Walking with my dog", task.getDescription());
+    }
 
 
 }
